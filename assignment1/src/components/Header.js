da312a94d3234715758904993c04a16c4b0bf127
kalyssao <klysssa@icloud.com>
2020-11-19 10:15:16 +0000

    header added

---
+++ assignment1/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <navbar>
+            <ul>
+                <li>Home</li>
+                <li>Sign Out</li>
+            </ul>
+        </navbar>
+    )
+}

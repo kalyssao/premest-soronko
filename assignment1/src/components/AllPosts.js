305d0eeb5d3c62d3f25ca306870158b49ef5ec8b
kalyssao <klysssa@icloud.com>
2020-11-23 10:27:17 +0000

    showing all posts

--- assignment1/src/components/AllPosts.js
+++ assignment1/src/components/AllPosts.js
@@ -2,16 +2,16 @@ import React from 'react'
 import Post from './Post'
 
 export default function AllPosts({ posts }) {
-    /*function truncateText(someText) {
-        if (someText.length > 30) {
-        }
-    }*/
-
+    
     return (
         <div className="allPosts">
             <h3>All Posts</h3>
             <ul className="postList">
-                {posts.map(post => (<Post key={post.id} /*truncatedPost={truncateText(post)}*/ post={post} />))}
+                {posts.map(
+                    post => (
+                    <Post key={post.id} post={post} />
+                    )
+                )}
             </ul> 
         </div>
     )

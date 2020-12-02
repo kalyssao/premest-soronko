58457ffe0f184b6bacf5fc00f228f7459f943329
kalyssao <klysssa@icloud.com>
2020-12-02 09:18:26 +0000

    added post router

--- assignment3/index.js
+++ assignment3/index.js
@@ -1,39 +1,48 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
-// Login
+const postRouter = require('./models/post')
+require('dotenv').config()
+
+app.use(express.json())
+
+const config = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(process.env.MONGO_DB_URI, config)
+.then(() => {
+    console.log('Successful connection!')
+})
+.catch((err) => {
+    console.log(err)
+})
+
+// Login endpoint
 app.post('/login', (req, res) => {
+    const username = req.query.username
+    const password = req.query.password
 
+    console.log(req)
+
+    if(username === "lyss" && password === "test"){
+        res.send('success')
+    } else {
+        res.status(400).send('wrong username or password')
+    }
 })
 
-// Get all blog posts
-app.get('/posts', (req, res) => {
+// Post endpoints
+app.use('/posts', postRouter)
 
-})
-
-// New post
-app.post('/new-post', (req, res) => {
-
-})
-
-// All posts by author
-app.get('/posts/:author', (req, res) => {
-
-})
 
 // Update blog post
 app.put('posts/:id', (req, res) => {
 
 })
 
-// Get post based on param
-
-
-// Delete blog post
-app.delete('/post/:id', (req, res) => {
-
-})
-
-app.listen(5000, () => {
-    console.log('Listening on port', 5000)
+app.listen(6000, () => {
+    console.log('Listening on port', 6000)
 })

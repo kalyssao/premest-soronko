cc2989210f18cfd0e4a1304aa96c5534a4290d5a
kalyssao <klysssa@icloud.com>
2020-12-10 19:17:12 +0000

    login authentication works

--- assignment4/controllers/admin.js
+++ assignment4/controllers/admin.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const adminRouter = express.Router()
+const authentication = require('../authentication')
 
 adminRouter.post('/login', (req, res) => {
-    
+    // Return a token that will be used later
+    const { username, password } = req.body
+    const token = authentication.generateAccessToken(username)
+    res.status(200).send({ "message": "successful login!", "token": token })
 })
 
 
 adminRouter.post('/logout', (req, res) => {
-    
+    // Remove said token
 })
 
 module.exports = adminRouter
